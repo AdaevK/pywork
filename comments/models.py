f0696ff6a3d28ad3760a98b3098a52253edaf400
Kirill Adaev <kv.adaev@yandex.ru>
2016-12-30 11:56:24 +0700

    added comments model and show comments list in comments page

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Comment(models.Model):
+    email = models.CharField(max_length=100)
+    body = models.TextField()
+    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.email
--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import Comment
 # Create your views here.
 
 def index(request):
-    return render(request, 'comments/index.html', {})
+    comments = Comment.objects.order_by('created_at')
+    context = {'comments': comments}
+    return render(request, 'comments/index.html', context)
